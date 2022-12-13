3fba0d7d741c60465a91f230ed7117f881f96c16
Henry Minden <henryminden@gmail.com>
2022-12-13 14:47:19 -0600

    Commit before switching to feature branch

--- ios/Yacht/ViewControllers/Addresses/AssetManagerViewController.swift
+++ ios/Yacht/ViewControllers/Addresses/AssetManagerViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class AssetManagerViewController: UIViewController {
 
+    var tokenAddress: String?
+    var supplyAPY: Double = 0
+    var borrowAPY: Double = 0
+    var symbolValue: String?
+    var deviceId: String?
+    var token: EulerToken?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Asset Manager"
         navigationController?.navigationBar.prefersLargeTitles = true
        
-        let reactView = getReactRootView(moduleName: "AssetManager")
-        
+        let reactView = getReactRootView(moduleName: "EulerAssetManager")
+        reactView.frame = self.view.frame
         self.view.addSubview(reactView)
     }
     
