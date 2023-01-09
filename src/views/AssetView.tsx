f4ff2596799c2f642ccdb553236e638fc1593ab7
hankminden <henryminden@gmail.com>
2023-01-09 10:14:07 -0600

    progress on deposits

--- src/views/AssetView.tsx
+++ src/views/AssetView.tsx
@@ -1,15 +1,40 @@
 import React, { useEffect } from "react";
-import { View, Dimensions } from "react-native";
+import { View, StyleSheet } from "react-native";
 import TokenCard  from "../components/TokenCard"
 import EulerStatusCard from "../components/EulerStatusCard"
 import { ethers } from "ethers";
+import YachtButton from "../components/YachtButton";
+import DepositEnable from "../components/DepositEnable";
 
 export default function EulerAssetManager() {
 
   return (
-  <View>
+  <View style={styles.mainContainer}>
     <TokenCard />
     <EulerStatusCard />
+    <View style={styles.buttonRow} >
+      <YachtButton type={'info'} onPress={() => console.log("Deposit")} title={'Deposit'} />
+      <YachtButton type={'info'} onPress={() => console.log("Borrow")} title={'Borrow'} />
+    </View>
+    <View style={styles.buttonRow} >
+      <YachtButton type={'info'} onPress={null} title={'Withdraw'} />
+      <YachtButton type={'info'} onPress={null} title={'Repay'} />
+    </View>
+    <DepositEnable />
   </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'column',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    marginTop: 10,
+    height: 50,
+    gap: 10
+  },
+
+});
